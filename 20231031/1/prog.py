81cab1924c6dba29b1fa304b2b059e7cc93a9fcf
Stephen <UsamUNX>
2023-11-01 16:22:46 +0300

    Home Task 1 20231031

---
+++ 20231031/1/prog.py
@@ -0,0 +1,19 @@
+class Omnibus:
+    d = {}
+    names = []
+
+    def __setattr__(self, name, value):
+        self.__class__.d[name] = self.__class__.d.setdefault(name, 0) + 1
+        self.names.append((name, id(self)))
+
+    def __getattr__(self, name):
+        if (name, id(self)) in self.names:
+            return self.__class__.d[name]
+
+    def __delattr__(self, name):
+        if (name, id(self)) in self.names:
+            self.__class__.d[name] -= 1
+            self.names.remove((name, id(self)))
+    
+import sys
+exec(sys.stdin.read())
